404beab8a729235397fbda2468963390364b8525
Ajay K P <ajay.cs23@bitsathy.ac.in>
2026-02-25 15:31:11 +0530

    Updated staff routes logic and path configurations

--- server/routes/staffRoutes.js
+++ server/routes/staffRoutes.js
@@ -6,10 +6,14 @@ const { verifyToken, authorizeRole } = require('../middlewares/authMiddleware');
 // All routes require PLACEMENT_STAFF role
 router.use(verifyToken, authorizeRole(['PLACEMENT_STAFF']));
 
+router.get('/profile', staffController.getProfile);
 router.post('/jobs/create', staffController.createJob);
 router.get('/applications/audit', staffController.getAuditApplications);
 router.get('/jobs/list', staffController.getPostedJobs);
 router.get('/jobs/:jobId/applicants', staffController.getJobApplicants);
 router.post('/applications/status', staffController.updateApplicationStatus);
+router.post('/jobs/shortlist', staffController.generateShortlist);
+router.get('/jobs/:jobId/shortlists', staffController.getShortlists);
+router.post('/applications/override', staffController.overrideEligibility);
 
 module.exports = router;
